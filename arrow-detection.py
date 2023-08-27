264f77f23b08c6f99c63fb307f224eeac2abf896
woojin <owj1207@gmail.com>
2023-08-27 23:58:34 +0900

    roboflow 모델 가져오는 코드 추가

--- arrow-detection.py
+++ arrow-detection.py
@@ -1,1 +1,10 @@
-import cv2 as cv
+from roboflow import Roboflow
+rf = Roboflow(api_key="7sg8pwBao1WlyoAmuk3W")
+project = rf.workspace().project("direction_detect")
+model = project.version(1).model
+
+# infer on a local image
+print(model.predict("imgs/arrow.png", confidence=5, overlap=30).json())
+
+# visualize your prediction
+# model.predict("imgs/arrow.png", confidence=5, overlap=30).save("prediction.jpg")
