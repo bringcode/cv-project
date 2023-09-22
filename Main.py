c74707872ff557f3a6f5f47ae98173d2fda4d51b
211_0@naver.com <211_0@naver.com>
2023-09-22 20:23:27 +0900

    'Main.py' 파일 추가

---
+++ Main.py
@@ -0,0 +1,10 @@
+# Main code
+from Core.Controller import Controller
+
+def main():
+    while not Controller.go_robo():   #  반환 값이 False일 때까지 계속 루프를 실행
+        continue
+
+
+if __name__ == "__main__":
+    main()
