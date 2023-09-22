4811c798b62e64d77c2e8da2274766627aaa0ded
211_0@naver.com <211_0@naver.com>
2023-09-22 19:16:49 +0900

    작년 코드에서 필요한 부분 추출

--- Motion/motion.py
+++ Motion/motion.py
@@ -76,3 +76,130 @@ class Motion:
                         continue
                 elif RX != 200:
                     self.distance = RX
+############################################################
+    # 기본자세 (100) -> 로봇을 기본 자세로 설정
+    def basic(self):
+        self.TX_data_py2(100)
+
+    # 걷기 (101~120)
+    def walk(self, dir, loop=1, sleep=0.1, short=False):
+        """
+        dir: {FORWARD, BACKWARD} - 로봇 이동 방향
+        loop: 반복 횟수
+        sleep: 각 동작 간 간격 (초)
+        short: 짧은 걸음 여부
+        """
+
+        """ parameter :
+        dir : {FORWARD, BACKWARD}
+        """
+        dir_list = {'FORWARD': 101, "BACKWARD": 111}
+        if short:
+            dir_list[dir] += 1
+
+        for _ in range(loop):
+            self.TX_data_py2(dir_list[dir])
+            time.sleep(sleep)
+
+    # 머리 각도 (121~140)
+    def set_head(self, dir, angle=0):
+        """
+        dir: {DOWN, LEFT, RIGHT, UPDOWN_CENTER, LEFTRIGHT_CENTER} - 머리 방향
+        angle: 머리 각도
+        """
+
+        """ parameter :
+        dir : {DOWN, LEFT, RIGHT, UPDOWN_CENTER, LEFTRIGHT_CENTER}
+        angle: {DOWN:{20,30,40,45,60,70,80,90,100,110},
+        LEFT:{30,45,60,90},
+        RIGHT:{30,45,60,90}
+        }
+        """
+        if dir == 'DOWN':
+            self.head_angle1 = angle
+        elif dir == 'LEFT' or dir == 'RIGHT':
+            self.head_angle2 = angle
+        elif dir == 'UPDOWN_CENTER':
+            self.head_angle1 = dir
+        elif dir == 'LEFTRIGHT_CENTER':
+            self.head_angle2 = dir
+        center_list = {'UPDOWN_CENTER': 140, 'LEFTRIGHT_CENTER': 135}
+        dir_list = {
+            'DOWN': {
+                20: 121, 30: 122, 45: 123, 50: 124, 60: 125, 70: 126, 80: 127, 90: 128, 100: 129, 110: 130
+            },
+            'LEFT': {
+                30: 134, 45: 133, 60: 132, 90: 131
+            },
+            'RIGHT': {
+                30: 136, 45: 137, 60: 138, 90: 139
+            }
+        }
+
+        if dir in center_list:
+            self.TX_data_py2(center_list[dir])
+        else:
+            self.TX_data_py2(dir_list[dir][angle])
+        time.sleep(0.3)
+
+    # 돌기 (141~160)
+    def turn(self, dir, angle, loop=1, sleep=0.5, arm=False):
+        """
+        dir: {LEFT, RIGHT} - 회전 방향
+        angle: 회전 각도
+        loop: 반복 횟수
+        sleep: 각 동작 간 간격 (초)
+        arm: 팔을 들고 회전 여부
+        """
+
+        """ parameter :
+        dir : {LEFT, RIGHT}
+        """
+        dir_list = {
+            "LEFT": {
+                10: 141, 20: 142, 45: 143, 60: 144
+            },
+            "RIGHT": {
+                10: 145, 20: 146, 45: 147, 60: 148
+            }
+        }
+
+        if arm:
+            if dir == "LEFT":
+                dir_list[dir][angle] += 7
+            elif dir == "RIGHT":
+                dir_list[dir][angle] += 6
+
+        for _ in range(loop):
+            self.TX_data_py2(dir_list[dir][angle])
+            time.sleep(sleep)
+
+    # 옆으로 이동 (161~170)
+    def walk_side(self, dir):
+        """
+        dir: {LEFT, RIGHT} - 이동 방향
+        """
+
+        """ parameter :
+        dir : {LEFT, RIGHT}
+        """
+        dir_list = {"LEFT": 161, "RIGHT": 169}
+        self.TX_data_py2(dir_list[dir])
+
+    # 위험지역 인식 (205~206)
+    def notice_area(self, area):
+        """
+        area: {BLACK, STAIR} - 위험 지역 정보
+        """
+
+        """parameter :
+        area : {BLACK, STAIR}
+        """
+        area_list = {'BLACK': 205, 'STAIR': 206}
+        self.TX_data_py2(area_list[area])
+    ############################################################
+
+
+if __name__ == '__main__':
+    motion = Motion()
+    motion.set_head("LEFTRIGHT_CENTER")
