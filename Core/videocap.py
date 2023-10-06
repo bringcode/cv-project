06acfca3eb07a3117418b266fed6b25817d3c56c
HyunWoo Cho <hwooo2255@gmail.com>
2023-10-06 20:45:39 +0900

    녹화 파일 수정

--- Core/videocap.py
+++ Core/videocap.py
@@ -1,13 +1,14 @@
 import picamera
 import time
+import cv2
 
 # 녹화 설정
 camera = picamera.PiCamera()
-camera.resolution = (800, 600)  # 해상도 설정 (원하는 해상도로 변경 가능)
+camera.resolution = (1280, 720)  # 해상도 설정 (원하는 해상도로 변경 가능)
 camera.framerate = 30            # 프레임 속도 설정 (원하는 속도로 변경 가능)
 
 # 바탕화면에 저장할 파일 경로
-output_file = "/home/pi/Desktop/20231006.h264"
+output_file = "/home/pi/Desktop/녹화된파일.h264"
 
 # 화면 미리보기 시작
 camera.start_preview()
@@ -30,10 +31,18 @@ try:
                 camera.stop_recording()
                 is_recording = False
             time.sleep(1)  # 다음 키 입력까지 대기
+        
+        # 녹화 중에 실시간 화면 보여주기
+        if is_recording:
+            frame = camera.capture_continuous(np.zeros((1280, 720, 3), dtype=np.uint8), format="bgr", use_video_port=True)
+            frame = next(frame)
+            cv2.imshow("실시간 녹화 화면", frame)
+            cv2.waitKey(1)
 except KeyboardInterrupt:
     # 사용자가 Ctrl+C를 누르면 녹화를 중지하고 종료합니다.
     if is_recording:
         camera.stop_recording()
     camera.stop_preview()
     camera.close()
+    cv2.destroyAllWindows()
 
