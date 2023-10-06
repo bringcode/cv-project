aa32d0f9995b3e419369c2ee3012869996a77735
HyunWoo Cho <hwooo2255@gmail.com>
2023-10-06 21:04:16 +0900

    화면 녹화 코드 수정

--- Core/videocap.py
+++ Core/videocap.py
@@ -2,12 +2,20 @@ import picamera
 import time
 import cv2
 import numpy as np
-from datetime import datetime  # datetime 모듈 추가
+from datetime import datetime
+import os
+
+# 저장 경로
+output_directory = "/home/pi/Desktop/record"
+
+# 저장 경로가 없으면 생성
+if not os.path.exists(output_directory):
+    os.makedirs(output_directory)
 
 # 녹화 설정
 camera = picamera.PiCamera()
-camera.resolution = (800, 600)  # 해상도 설정 (원하는 해상도로 변경 가능)
-camera.framerate = 30            # 프레임 속도 설정 (원하는 속도로 변경 가능)
+camera.resolution = (800, 600)  # 해상도 설정
+camera.framerate = 30
 
 # 화면 미리보기 시작
 camera.start_preview()
@@ -15,9 +23,10 @@ camera.start_preview()
 # 녹화 플래그 초기화
 is_recording = False
 
+# 파일명 생성 함수
 def get_current_time():
     now = datetime.now()
-    timestamp = now.strftime("%Y%m%d_%H%M%S")  # 년월일_시분초 형식의 시간 정보 생성
+    timestamp = now.strftime("%Y%m%d_%H%M%S")
     return timestamp
 
 try:
@@ -25,22 +34,29 @@ try:
         key = input("q를 눌러 녹화를 시작 또는 중지하세요: ")
         if key == "q":
             if not is_recording:
-                timestamp = get_current_time()  # 현재 시간 정보 가져오기
-                output_file = f"/home/pi/Desktop/record/{timestamp}_녹화된파일.h264"  # 파일명에 시간 추가
+                timestamp = get_current_time()
+                output_file = f"{output_directory}/{timestamp}_녹화된파일.h264"
                 print(f"{timestamp}에 녹화를 시작합니다.")
-                # 녹화 시작
                 camera.start_recording(output_file)
                 is_recording = True
             else:
                 print("녹화를 중지합니다.")
-                # 녹화 중지
                 camera.stop_recording()
                 is_recording = False
-            time.sleep(1)  # 다음 키 입력까지 대기
+            time.sleep(1)
+        
+        if is_recording:
+            # 실시간 화면 표시
+            frame = np.empty((camera.resolution[1] * camera.resolution[0] * 3,), dtype=np.uint8)
+            camera.capture(frame, 'bgr', use_video_port=True)
+            frame = frame.reshape((camera.resolution[1], camera.resolution[0], 3))
+            
+            cv2.imshow("실시간 녹화 화면", frame)
+            cv2.waitKey(1)
 
 except KeyboardInterrupt:
-    # 사용자가 Ctrl+C를 누르면 녹화를 중지하고 종료합니다.
     if is_recording:
         camera.stop_recording()
     camera.stop_preview()
     camera.close()
+    cv2.destroyAllWindows()
