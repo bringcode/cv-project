56c8fba2f22005dbfe3f4b29ee4b48ad4925269b
HyunWoo Cho <hwooo22@kookmin.ac.kr>
2023-10-09 02:26:28 +0900

    Ctrl 부분 세세하게 구분 및 주석 처리

--- EWStest/Core/Controller.py
+++ EWStest/Core/Controller.py
@@ -9,19 +9,23 @@ from Setting import cur
 import time
 
 class Act(Enum):
-    START = auto()
-    SEARCH_FIRST = auto()
-    SEARCH_ARROW = auto()
-    SEARCH_FLAG = auto()
-    EXIT = auto()
+    START = auto() # 시작
+    SEARCH_FIRST = auto() # T샷 시작
+    SEARCH_BALL = auto() # 공 찾기
+    SEARCH_ARROW = auto() # 화살표 찾기
+    SEARCH_FLAG = auto() # 깃발 찾기
+    PUTTING = auto() # 공 퍼팅
+    CHECK = auto() # 홀인 확인
+    EXIT = auto() # 종료
 
 
+# 상황 판단 하는 파트
 class Controller:
-
     robo: Robo = Robo()
     act: Act = Act.START
-    count_putting: int = 0
-    check_holein: int = 0
+
+    count_putting: int = 0 # 퍼팅 횟수
+    check_holein: int = 0 # 홀인 판단 횟수
     area: str = "" # 현재 맵
 
 
@@ -32,7 +36,27 @@ class Controller:
     _check: Missionchecking = Missionchecking()
     _putt: Missionputting = Missionputting()
 
+    # 처음 공이 어디에 있는지 확인하는 코드
+    @classmethod
+    def check_ball_first(self):
+        time.sleep(1)
+        
+        # 이 부분에 첫 공을 찾는 부분을 넣어야하는게 맞는지?
 
+    # 퍼팅 후 공이 나갔는지 확인하는 코드 (공을 발견하면 그 각도로 멈춤) (발견 못하면 나갔다고 판단)
+    @classmethod
+    def check_ball_out(self):
+        time.sleep(1)
+
+    # 퍼팅 후 공 위치가 위험한지 안 위험한지
+    @classmethod
+    def check_ball_location(self):
+        time.sleep(1)
+
+    # 홀인 했는지 안 했는지
+    @classmethod
+    def check_ball_in(self):
+        time.sleep(1)
 
 
     
