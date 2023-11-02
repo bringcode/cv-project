2145cb094ee21de9a1acef43d80dee021143cd5d
211_0@naver.com <211_0@naver.com>
2023-11-02 21:28:02 +0900

    경로 수정

--- EWStest/Core/Robo.py
+++ EWStest/Core/Robo.py
@@ -2,10 +2,10 @@
 # from Sensor.ImageProcessor import ImageProccessor
 from Motion.Motion import Motion
 from Setting import Setting
-from EWStest.Sensor.ball_y_center import BallCenterMeasurer
+from Sensor.ball_y_center import BallCenterMeasurer
 
 
-print('code: Robo.py - ## Debug')
+print("code: Robo.py - ## Debug")
 
 
 class Robo:
@@ -14,9 +14,9 @@ class Robo:
     black_room_list = list = []
     alphabet_color: str
     box_pos: int = 4
-    feet_down = 'LEFT_DOWN'
+    feet_down = "LEFT_DOWN"
     # _image_processor = ImageProccessor(video="")  # Image Processor
 
-    def __init__(self, vpath=''):
+    def __init__(self, vpath=""):
         # self._image_processor = ImageProccessor(video=vpath) # Image Processor
         self._motion = Motion()  # Motion
