8c1b14dfb43da24c04abcb83dba4241cfddbde8c
211_0@naver.com <211_0@naver.com>
2023-11-02 21:33:35 +0900

    경로 수정

--- EWStest/Core/Controller.py
+++ EWStest/Core/Controller.py
@@ -6,20 +6,22 @@ from Core.SearchBall import SearchBall
 from Core.Putting import Putting
 from Core.Check import Check
 from Sensor.search_ball import FindBall
-from EWStest.Sensor.ball_y_center import BallCenterMeasurer
-#from Setting import cur
+from Sensor.ball_y_center import BallCenterMeasurer
+
+# from Setting import cur
 import time
 
+
 class Act(Enum):
-    START = auto() # 시작 - 아무도 동작도 안 함
-    SEARCH_FIRST = auto() # T샷 시작
-    SEARCH_BALL = auto() # 공 찾기
-    SEARCH_FLAG = auto() # 깃발 찾기
-    SEARCH_ARROW = auto() # 화살표 찾기
-    SEARCH_BUNKER = auto() # 벙커 찾기
-    PUTTING = auto() # 공 퍼팅
-    CHECK = auto() # 홀인 확인
-    EXIT = auto() # 종료
+    START = auto()  # 시작 - 아무도 동작도 안 함
+    SEARCH_FIRST = auto()  # T샷 시작
+    SEARCH_BALL = auto()  # 공 찾기
+    SEARCH_FLAG = auto()  # 깃발 찾기
+    SEARCH_ARROW = auto()  # 화살표 찾기
+    SEARCH_BUNKER = auto()  # 벙커 찾기
+    PUTTING = auto()  # 공 퍼팅
+    CHECK = auto()  # 홀인 확인
+    EXIT = auto()  # 종료
 
 
 # 상황 판단 하는 파트
@@ -27,18 +29,17 @@ class Controller:
     robo: Robo = Robo()
     act: Act = Act.START
 
-    count_putting: int = 0 # 퍼팅 횟수
-    check_holein: int = 0 # 홀인 판단 횟수
-    area: str = "" # 현재 맵
+    count_putting: int = 0  # 퍼팅 횟수
+    check_holein: int = 0  # 홀인 판단 횟수
+    area: str = ""  # 현재 맵
     ball: bool
-
 
     # Misson.py
     _first: SearchFirst = SearchFirst()
     _find: SearchBall = SearchBall()
     _check: Check = Check()
     _putt: Putting = Putting()
-    
+
     # 처음 공이 어디에 있는지 확인하는 코드
     @classmethod
     def check_ball_first(self):
@@ -46,10 +47,10 @@ class Controller:
         time.sleep(3)
         dir = 100
 
-        ballFunction = FindBall() # Search_ball 함수 
-        is_ball_find = ballFunction.process() # process 가져옴 True / False로 반환됨.
+        ballFunction = FindBall()  # Search_ball 함수
+        is_ball_find = ballFunction.process()  # process 가져옴 True / False로 반환됨.
 
-        for i in range (3):
+        for i in range(3):
             if is_ball_find == False:
                 dir -= 10
                 self.robo._motion.set_head("DOWN", dir)
@@ -62,7 +63,7 @@ class Controller:
                 print("왼쪽 위치에 있지 않거나, 문제가 있을 수 있습니다.")
                 print("로봇이 가운데 위치한다고 생각하고 시작하겠습니다.")
 
-        if is_ball_find != True:        
+        if is_ball_find != True:
             self.robo._motion.set_head("RIGHT", 45)
             time.sleep(2)
             is_ball_find = ballFunction.process()
@@ -85,11 +86,6 @@ class Controller:
                         print("공을 처음 시작할 때 어디서도 찾지 못했습니다.")
                     else:
                         print("True False가 반환되지 않았습니다.")
-                            
-                        
-                       
-
-            
 
         # 로봇이 왼쪽에서 시작한다고 생각하고 시작하는 부분
 
@@ -131,32 +127,32 @@ class Controller:
         # 이제 10도를 더 내려서 가운데 확인
         # 오른쪽 확인
         # 왼쪽 확인
-        
+
         # 이 부분에 첫 공을 찾는 부분을 넣어야하는게 맞는지?
 
     @classmethod
     def ball_feature_ball(self):
         print("Debug in ball_feature_ball")
         time.sleep(1)
-        ball_feature = ['None','None','None']  # [공의 가운데 여부, 공의 x중심좌표, 공의 y중심좌표]
+        ball_feature = ["None", "None", "None"]  # [공의 가운데 여부, 공의 x중심좌표, 공의 y중심좌표]
         # ball_ball_feature_measure 에서 return 값: L / C / R
-        while ball_feature[0] != 'C':
+        while ball_feature[0] != "C":
             cmeasurer = BallCenterMeasurer()
             ball_feature = cmeasurer.process()
             print(ball_feature[0])
 
-            if ball_feature[0] == 'L':
+            if ball_feature[0] == "L":
                 time.sleep(0.5)
                 print("공이 왼쪽에 있습니다.")
                 self.robo._motion.walk_side("LEFT")
                 time.sleep(2)
-            
-            elif ball_feature[0] == 'C':
+
+            elif ball_feature[0] == "C":
                 time.sleep(0.5)
                 print("공이 가운데 있습니다.")
                 break
 
-            elif ball_feature[0] == 'R':
+            elif ball_feature[0] == "R":
                 time.sleep(0.5)
                 print("공이 오른쪽에 있습니다.")
                 self.robo._motion.walk_side("RIGHT")
@@ -169,8 +165,7 @@ class Controller:
     @classmethod
     def check_ball_out(self):
         # 위험 지역에 공이 있으면 공이 나간 걸로 판단 -> 위험 지역을 판별할 cv 생각해야 함
-        
-            
+
         time.sleep(1)
 
     # 퍼팅 후 공 위치가 위험한지 안 위험한지
@@ -184,14 +179,12 @@ class Controller:
         # if 홀컵의 오른쪽이면 공의 왼쪽에서 칠 수 있게 -> 홀컵을 인식하고, 홀컵 기준 오른쪽에서 왼쪽 방향으로 치기
         # elif 홀컵의 왼쪽이면 공의 오른쪽에서 칠 수 있게 -> 홀컵을 인식하고, 홀컵 기준 왼쪽에서 오른쪽 방향으로 치기
         # elif 홀선과 일직선이면 홀컵 쪽으로 치기
-        # else 판단 불가 False        
-                
+        # else 판단 불가 False
 
     # 홀인 했는지 안 했는지
     @classmethod
     def check_ball_in(self):
         time.sleep(1)
-
 
     @classmethod
     def go_robo(self):
@@ -202,7 +195,7 @@ class Controller:
         time.sleep(1)
         self.ball_feature_ball()
         time.sleep(2)
-        
+
         self.act = act.START
 
         if act == act.START:
@@ -212,50 +205,48 @@ class Controller:
             self.act = act.SEARCH_FIRST
 
         elif act == act.SEARCH_FIRST:
-            print("ACT: ", act ) # Debug
+            print("ACT: ", act)  # Debug
             time.sleep(0.5)
 
             self.check_ball_first()
             self.act = act.SEARCH_BALL
 
         elif act == act.SEARCH_BALL:
-            print("Act:", act) # Debug
+            print("Act:", act)  # Debug
             time.sleep(0.5)
 
             self.ball_feature_ball()
             self.act = act.SEARCH_FLAG
 
         elif act == act.SEARCH_FLAG:
-            print("Act:", act) # Debug
+            print("Act:", act)  # Debug
 
             self.act = act.SEARCH_ARROW
 
         elif act == act.SEARCH_ARROW:
-            print("Act:", act) # Debug
+            print("Act:", act)  # Debug
 
             self.act = act.SEARCH_BUNKER
 
         elif act == act.SEARCH_BUNKER:
-            print("Act:", act) # Debug
+            print("Act:", act)  # Debug
 
-            
             self.act = act.PUTTING
 
         elif act == act.PUTTING:
-            print("Act:", act) # Debug
+            print("Act:", act)  # Debug
 
             self.act = act.CHECK
 
         elif act == act.CHECK:
-            print("Act:", act) # Debug
+            print("Act:", act)  # Debug
 
             self.act = act.EXIT
 
         elif act == act.EXIT:
-            print("Act:", act) # Debug
+            print("Act:", act)  # Debug
 
         else:
             print("이쪽으로 빠지면 문제가 있는거임.")
-    
+
         return False
-    
