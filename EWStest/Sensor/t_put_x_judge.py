808e099e2fb8d6c7cdf410205b2f43a0519c91f1
211_0@naver.com <211_0@naver.com>
2023-11-06 17:29:58 +0900

    테스트를 위한 imshow

--- EWStest/Sensor/t_put_x_judge.py
+++ EWStest/Sensor/t_put_x_judge.py
@@ -4,10 +4,10 @@
 import numpy as np
 import cv2
 
-class Tputting_x_BallCenterMeasurer:
 
+class Tputting_x_BallCenterMeasurer:
     def __init__(self, img_width=800, img_height=600, width=4, focal=450):
-        self.dist = 0 
+        self.dist = 0
         self.focal = focal
         self.pixels = 30
         self.width = width
@@ -17,7 +17,7 @@ class Tputting_x_BallCenterMeasurer:
         self.img_width_middle = img_width // 2
         self.img_height_middle = img_height // 2
 
-        self.kernel = np.ones((3, 3), 'uint8')
+        self.kernel = np.ones((3, 3), "uint8")
         self.font = cv2.FONT_HERSHEY_SIMPLEX
         self.org = (0, 20)
         self.fontScale = 0.6
@@ -55,17 +55,18 @@ class Tputting_x_BallCenterMeasurer:
     # 가운데인지 판단하는 코드
     def judgeMiddle(self, max_x, min_x):
         up_dist = min_x  # l_dist: 공을 표시한 박스 가장 왼쪽으로부터 영상 가장 왼쪽 끝까지의 거리
-        down_dist = self.img_width - max_x  # r_dist: 공을 표시한 박스 가장 오른쪽으로부터 영상 가장 오른쪽 끝까지의 거리
-        
-        error_range = self.img_width//12 # 오차 허용 범위
+        down_dist = (
+            self.img_width - max_x
+        )  # r_dist: 공을 표시한 박스 가장 오른쪽으로부터 영상 가장 오른쪽 끝까지의 거리
+
+        error_range = self.img_width // 12  # 오차 허용 범위
 
         # 박스가 영상의 왼쪽 오른쪽 끝 부분과 떨어진 거리가 오차 허용 범위(error_range) 이내일 때, True를 is_Middle에 저장
         is_Middle = abs(up_dist - down_dist) < error_range
         return is_Middle
 
-
     def process(self):
-        cap = cv2.VideoCapture(0,cv2.CAP_V4L) # 인자로 있었는데 몰루? -> cv2.CAP_V4L
+        cap = cv2.VideoCapture(0, cv2.CAP_V4L)  # 인자로 있었는데 몰루? -> cv2.CAP_V4L
         # cv2.namedWindow('Object Dist Measure ', cv2.WINDOW_NORMAL)
         # cv2.resizeWindow('Object Dist Measure ', 700, 600)
 
@@ -76,8 +77,8 @@ class Tputting_x_BallCenterMeasurer:
                 break
             img = cv2.dilate(img, self.kernel, iterations=1)
             hsv_img = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
-            
-            #window version
+
+            # window version
             # lower = np.array([170, 99, 100])
             # upper = np.array([180, 255, 255])
             # mask = cv2.inRange(hsv_img, lower, upper)
@@ -89,7 +90,7 @@ class Tputting_x_BallCenterMeasurer:
             # upper_flag = np.array([45, 255, 255])
             # mask_flag = cv2.inRange(hsv_img, lower_flag, upper_flag)
 
-            #mac version
+            # mac version
             # lower = np.array([170, 100, 100])
             # upper = np.array([180, 255, 255])
             # mask = cv2.inRange(hsv_img, lower, upper)
@@ -103,46 +104,53 @@ class Tputting_x_BallCenterMeasurer:
             # upper_flag = np.array([35, 255, 255])
             # mask_flag = cv2.inRange(hsv_img, lower_flag, upper_flag)
 
+            # 모폴로지 연산
+            d_img = cv2.morphologyEx(mask, cv2.MORPH_OPEN, self.kernel, iterations=5)
 
-            #모폴로지 연산
-            d_img = cv2.morphologyEx(mask, cv2.MORPH_OPEN, self.kernel, iterations = 5)
-            
-            cont,hei = cv2.findContours(d_img,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
-            cont = sorted(cont, key = cv2.contourArea, reverse = True)[:1]
+            cont, hei = cv2.findContours(
+                d_img, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE
+            )
+            cont = sorted(cont, key=cv2.contourArea, reverse=True)[:1]
 
             max_x, min_x, max_y, min_y = -1, self.img_width + 1, -1, self.img_width + 1
             ball_box = None
-            
-            for cnt in cont:
-                if (cv2.contourArea(cnt)>100 and cv2.contourArea(cnt)<306000):
 
+            for cnt in cont:
+                if cv2.contourArea(cnt) > 100 and cv2.contourArea(cnt) < 306000:
                     rect = cv2.minAreaRect(cnt)
                     ball_box = cv2.boxPoints(rect)
                     ball_box = np.int0(ball_box)
                     # print('points :', ball_box)
-                    cv2.drawContours(img,[ball_box], -1,(255,0,0),3)
+                    cv2.drawContours(img, [ball_box], -1, (255, 0, 0), 3)
 
                     max_x, min_x, max_y, min_y = self.getMaxMin(ball_box)
                     ball_x_isMiddle = self.judgeMiddle(max_x, min_x)
 
-                #font = cv2.FONT_HERSHEY_SIMPLEX 
-                #org = (0,20)  
-                #fontScale = 0.6 
-                #color = (0, 0, 255) 
-                #thickness = 2
+                font = cv2.FONT_HERSHEY_SIMPLEX
+                org = (0, 20)
+                fontScale = 0.6
+                color = (0, 0, 255)
+                thickness = 2
 
-                #image = cv2.putText(img, 'flag Middle : {}'.format(ball_x_isMiddle), org, font,  
-                #1, color, 2, cv2.LINE_AA)
+                image = cv2.putText(
+                    img,
+                    "flag Middle : {}".format(ball_x_isMiddle),
+                    org,
+                    font,
+                    1,
+                    color,
+                    2,
+                    cv2.LINE_AA,
+                )
 
-#            cv2.imshow('Object Dist Measure ', img)
- #           if cv2.waitKey(1) & 0xFF == ord('q'):
-                #break
+            cv2.imshow("Object Dist Measure ", img)
+            if cv2.waitKey(1) & 0xFF == ord("q"):
+                break
 
-  #      cv2.destroyAllWindows()
-                    
-                    return ball_x_isMiddle  # imshow 하려함 => 위에 있는 주석을 활성화하고, return은 주석처리
-            return False
+            cv2.destroyAllWindows()
 
+            #         return ball_x_isMiddle  # imshow 하려함 => 위에 있는 주석을 활성화하고, return은 주석처리
+            # return False
 
 
 if __name__ == "__main__":
