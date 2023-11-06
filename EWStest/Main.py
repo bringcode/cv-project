354645d603d19799e204a75833487336bf3e3d43
HyunWoo Cho <hwooo2255@gmail.com>
2023-11-06 21:15:47 +0900

    Debug

--- EWStest/Main.py
+++ EWStest/Main.py
@@ -14,10 +14,11 @@ if __name__ == "__main__":
     Motion = Motion()
     # Motion.TX_data_py3(123)
     print("head right")
-    time.sleep(3)
     Motion.TX_data_py3(177)  # 1도우향
     time.sleep(3)
+    print("head right")
     Motion.TX_data_py3(177)
     time.sleep(3)
+    print("head right")
     Motion.TX_data_py3(177)
     # main()
