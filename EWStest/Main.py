09350fb9f4bd9b933c60c5f727aecbf73cd896e6
211_0@naver.com <211_0@naver.com>
2023-11-06 21:37:27 +0900

    테스트용

--- EWStest/Main.py
+++ EWStest/Main.py
@@ -13,12 +13,12 @@ def main():
 if __name__ == "__main__":
     Motion = Motion()
     # Motion.TX_data_py3(123)
-    print("head right")
-    Motion.TX_data_py3(177)  # 1도우향
-    time.sleep(3)
-    print("head right")
-    Motion.TX_data_py3(177)
-    time.sleep(3)
-    print("head right")
-    Motion.TX_data_py3(177)
-    # main()
+    # print("head right")
+    # Motion.TX_data_py3(177)  # 1도우향
+    # time.sleep(3)
+    # print("head right")
+    # Motion.TX_data_py3(177)
+    # time.sleep(3)
+    # print("head right")
+    # Motion.TX_data_py3(177)
+    main()
