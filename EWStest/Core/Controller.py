8cece17c1f7dd108a14cb954e46a0652cb8b66a8
211_0@naver.com <211_0@naver.com>
2023-11-08 03:04:07 +0900

    테스트 - 초기 각도 조절

--- EWStest/Core/Controller.py
+++ EWStest/Core/Controller.py
@@ -575,7 +575,7 @@ class Controller:
         canPutting = self.canPutting
 
         self.act = act.START
-        self.robo._motion.set_head("DOWN", 60)  # test
+        self.robo._motion.set_head("DOWN", 45)  # test
         time.sleep(1)  # test
         self.check_ball_distance()  # test
         time.sleep(10)  # test
