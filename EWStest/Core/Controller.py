6cda8eb2ba3b1dc1907aa6b251f4db99c240b6f2
HyunWoo Cho <hwooo2255@gmail.com>
2023-11-08 01:25:54 +0900

    test

--- EWStest/Core/Controller.py
+++ EWStest/Core/Controller.py
@@ -578,6 +578,7 @@ class Controller:
 
         self.act = act.START
         self.robo._motion.set_head("DOWN",30) # test
+        time.sleep(1)
         self.check_ball_distance() # test
         time.sleep(10) # test
 
