f66b65f46acdec30a2db0cdaea6b3ac397f74189
daeheejang <84303422+daeheejang@users.noreply.github.com>
2023-11-07 19:24:37 +0900

    Update yellowObjectdetect.py

--- yellowObjectdetect.py
+++ yellowObjectdetect.py
@@ -1,12 +1,13 @@
 import numpy as np
 import cv2
 
-your_area_threshold = 300  # 사용자 정의 임계값, 필요에 따라 값을 조정
+your_area_threshold = 300  # 사용자 정의 임계값, 필요에 따라 값을 조정하세요
 
-cap = cv2.VideoCapture('YYY.h264')  # 비디오 파일 경로를 설정하십시오.
+cap = cv2.VideoCapture('YYY.h264')  # 비디오 파일 경로를 설정하세요
 
 # 초록 영역 박스의 정보를 저장할 리스트
 green_boxes = []
+farthest_flag_box = None
 
 while True:
     ret, frame = cap.read()
@@ -39,7 +40,8 @@ while True:
     # 노랑색 범위에 해당하는 부분을 추출
     yellow_mask = cv2.inRange(hsv_frame, low_yellow, high_yellow)
 
-    # 초록 박스 내부에 있는 노랑 영역만 남기고 나머지 부분 제거
+    shape_info_list = []
+
     for green_box in green_boxes:
         x, y, w, h = green_box
         yellow_roi = yellow_mask[y:y + h, x:x + w]
@@ -49,32 +51,84 @@ while True:
 
         for i in range(1, len(stats)):
             x_blob, y_blob, w_blob, h_blob, area_blob = stats[i]
-            if area_blob > your_area_threshold:
-                cv2.rectangle(frame, (x + x_blob, y + y_blob), (x + x_blob + w_blob, y + y_blob + h_blob), (0, 255, 0), 2)
 
-                # Convert the yellow region into a binary image for contour detection
-                yellow_binary = np.zeros_like(yellow_roi)
-                yellow_binary[y_blob:y_blob + h_blob, x_blob:x_blob + w_blob] = yellow_roi[y_blob:y_blob + h_blob, x_blob:x_blob + w_blob]
+            # 영역값이 100픽셀 이하인 영역을 제거
+            if area_blob <= 100:
+                continue
 
-                # Find contours in the binary image
-                yellow_contours, _ = cv2.findContours(yellow_binary, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+            cv2.rectangle(frame, (x + x_blob, y + y_blob), (x + x_blob + w_blob, y + y_blob + h_blob), (0, 255, 0), 2)
 
-                for contour in yellow_contours:
-                    # Approximate the contour to find the vertices
-                    epsilon = 0.04 * cv2.arcLength(contour, True)
-                    approx = cv2.approxPolyDP(contour, epsilon, True)
-                    num_vertices = len(approx)
+            # Convert the yellow region into a binary image for contour detection
+            yellow_binary = np.zeros_like(yellow_roi)
+            yellow_binary[y_blob:y_blob + h_blob, x_blob:x_blob + w_blob] = yellow_roi[y_blob:y_blob + h_blob, x_blob:x_blob + w_blob]
 
-                    # Display the shape as ARROW or FLAG based on the number of vertices
-                    shape_text = "ARROW" if 6<= num_vertices <= 8 else "FLAG"
-                    cv2.putText(frame, f'Shape: {shape_text}', (x + x_blob, y + y_blob - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
+            # Find contours in the binary image
+            yellow_contours, _ = cv2.findContours(yellow_binary, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
-    # 원본 영상을 표시
+            for contour in yellow_contours:
+                # Approximate the contour to find the vertices
+                epsilon = 0.04 * cv2.arcLength(contour, True)
+                approx = cv2.approxPolyDP(contour, epsilon, True)
+                num_vertices = len(approx)
+
+                # Display the shape as ARROW or FLAG based on the number of vertices
+                shape_text = "ARROW" if 7 <= num_vertices <= 8 else "FLAG"
+
+                # Calculate the center of the yellow region
+                center_x = x + x_blob + w_blob // 2
+                center_y = y + y_blob + h_blob // 2
+                center = (center_x, center_y)
+
+                # Add shape information to the list
+                shape_info_list.append((center, shape_text))
+
+    # 사용자 정의 조건
+    custom_condition = True
+
+    if custom_condition:
+        # FLAG로 인식된 박스의 개수가 2개 이상인 경우
+        flag_boxes = [box for box in shape_info_list if box[1] == "FLAG"]
+        if len(flag_boxes) >= 2:
+            # 카메라 화면의 중하단 중앙 좌표
+            camera_center = (frame.shape[1] // 2, frame.shape[0])
+
+            # Find the farthest FLAG box among FLAG boxes
+            max_distance = 0
+
+            for box in flag_boxes:
+                box_center = box[0]
+                distance = ((box_center[0] - camera_center[0]) ** 2 + (box_center[1] - camera_center[1]) ** 2) ** 0.5
+
+                if distance > max_distance:
+                    max_distance = distance
+                    farthest_flag_box = box
+
+            # Change the rest of the FLAG boxes to ARROW
+            for i, box in enumerate(shape_info_list):
+                if box[1] == "FLAG" and box != farthest_flag_box:
+                    shape_info_list[i] = (box[0], "ARROW")
+
+    # Print the center coordinates
+    if farthest_flag_box is not None:
+        farthest_center = farthest_flag_box[0]
+        print("Farthest FLAG Center: {farthest_center}")
+
+    # Display centers and shape information on the frame
+    for shape_info in shape_info_list:
+        center, shape_text = shape_info[0], shape_info[1]
+        offset = 10  # Offset to move the text upward
+        if shape_text == "FLAG":
+            cv2.putText(frame, f'Shape: {shape_text}', (center[0], center[1] - offset), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
+        else:
+            cv2.putText(frame, f'Shape: {shape_text}', (center[0], center[1] + offset), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
+
+    # Display the original frame
     cv2.imshow('Green and Yellow Frame', frame)
 
     key = cv2.waitKey(1) & 0xFF
     if key == ord('q'):
         break
 
+# Release the video capture and close all OpenCV windows
 cap.release()
 cv2.destroyAllWindows()
