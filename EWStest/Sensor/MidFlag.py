a4ea5596bf4fb5f1230ca8d15b61ecc404b351d2
daeheejang <84303422+daeheejang@users.noreply.github.com>
2023-11-08 03:41:20 +0900

    Update MidFlag.py

--- EWStest/Sensor/MidFlag.py
+++ EWStest/Sensor/MidFlag.py
@@ -2,13 +2,14 @@ import numpy as np
 import cv2
 
 class ShapeRecognition:
-    def __init(self, video_path):
+    def __init__(self, video_path):
         self.cap = cv2.VideoCapture(video_path, cv2.CAP_V4L)
         if not self.cap.isOpened():
             raise ValueError(f"Video at {video_path} cannot be opened")
         self.green_boxes = []
         self.flags = []  # List to store recognized flags
         self.arrows = []  # List to store recognized arrows
+        self.farthest_flag_box = None
 
     def process_frame(self, frame):
         hsv_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
@@ -26,16 +27,14 @@ class ShapeRecognition:
         high_yellow = np.array([43, 184, 255])
         yellow_mask = cv2.inRange(hsv_frame, low_yellow, high_yellow)
 
-        # Reset flag detection
-        flag_detected = False
-        self.farthest_flag_box = None
-
         # For each green box, detect yellow shapes within it
         for green_box in self.green_boxes:
             x, y, w, h = green_box
             green_roi = frame[y:y+h, x:x+w]
             yellow_roi_mask = yellow_mask[y:y+h, x:x+w]
             yellow_contours, _ = cv2.findContours(yellow_roi_mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+            
+            flag_detected = False  # Flag detection flag
             
             for cnt in yellow_contours:
                 approx = cv2.approxPolyDP(cnt, 0.03 * cv2.arcLength(cnt, True), True)
@@ -66,6 +65,21 @@ class ShapeRecognition:
                             self.flags.append((cx, cy, 'FLAG'))
                             flag_detected = True
 
+            # Calculate the distance of the center of the flag box from the bottom center of the camera frame
+            if flag_detected:
+                camera_center = (frame.shape[1] // 2, frame.shape[0])
+                flag_center = (x + cx, y + cy)
+                distance = abs(flag_center[1] - camera_center[1])
+
+                # If this flag is farther than the previously detected farthest flag, update it
+                if self.farthest_flag_box is None or distance > self.farthest_flag_box[3]:
+                    self.farthest_flag_box = (cx, cy, 'FLAG', distance)
+
+        # After processing all green boxes, update the rest of the FLAG boxes to ARROW
+        for i, box in enumerate(self.flags):
+            if box[2] == 'FLAG' and box != self.farthest_flag_box:
+                self.flags[i] = (box[0], box[1], 'ARROW')
+
         return frame
 
     def run(self):
@@ -87,7 +101,7 @@ class ShapeRecognition:
         self.cap.release()
         cv2.destroyAllWindows()
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     video_path = 0  # Use 0 for webcam
     shape_recognition = ShapeRecognition(video_path)
     shape_recognition.run()
