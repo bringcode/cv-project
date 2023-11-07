99d93bb5787f15982f253810cb25bef0ee25d365
daeheejang <84303422+daeheejang@users.noreply.github.com>
2023-11-08 04:07:05 +0900

    Update MidFlag.py

--- EWStest/Sensor/MidFlag.py
+++ EWStest/Sensor/MidFlag.py
@@ -3,7 +3,7 @@ import cv2
 
 class ShapeRecognition:
     def __init__(self, video_path):
-        self.cap = cv2.VideoCapture(video_path)
+        self.cap = cv2.VideoCapture(video_path, cv2.CAP_V4L)
         if not self.cap.isOpened():
             raise ValueError(f"Video at {video_path} cannot be opened")
         self.green_boxes = []
@@ -13,7 +13,7 @@ class ShapeRecognition:
     def process_frame(self, frame):
         hsv_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 
-        # Green range definition
+        # 녹색 범위 정의
         low_green = np.array([57, 78, 61])
         high_green = np.array([89, 255, 255])
         green_mask = cv2.inRange(hsv_frame, low_green, high_green)
@@ -21,47 +21,21 @@ class ShapeRecognition:
         contours, _ = cv2.findContours(green_mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
         self.green_boxes = [cv2.boundingRect(contour) for contour in contours]
 
-        # Yellow range definition
+        # 노랑색 범위 정의
         low_yellow = np.array([0, 16, 144])
         high_yellow = np.array([43, 184, 255])
         yellow_mask = cv2.inRange(hsv_frame, low_yellow, high_yellow)
 
-        # For each green box, detect yellow shapes within it
+        max_flag_y = -1  # 가장 높은 FLAG의 y 좌표 초기화
+        flag_detected = False  # Flag detection flag
+
         for green_box in self.green_boxes:
             x, y, w, h = green_box
             green_roi = frame[y:y+h, x:x+w]
             yellow_roi_mask = yellow_mask[y:y+h, x:x+w]
             yellow_contours, _ = cv2.findContours(yellow_roi_mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-            
-            flag_detected = False  # Flag detection flag
-            
-            # Yellow contour combination
-            centers = []
-            indices_to_combine = []
 
-            for i, cnt in enumerate(yellow_contours):
-                M = cv2.moments(cnt)
-                if M['m00'] != 0:
-                    cx = int(M['m10'] / M['m00'])
-                    cy = int(M['m01'] / M['m00'])
-                    centers.append((cx, cy))
-                    indices_to_combine.append(i)
-
-            for i in range(len(centers)):
-                if i in indices_to_combine:
-                    for j in range(i + 1, len(centers)):
-                        if j in indices_to_combine:
-                            distance = np.sqrt((centers[i][0] - centers[j][0]) ** 2 + (centers[i][1] - centers[j][1]) ** 2)
-                            if distance < 10:
-                                yellow_contours[i] = np.concatenate((yellow_contours[i], yellow_contours[j]))
-                                indices_to_combine.remove(j)
-
-            yellow_mask_combined = np.zeros_like(yellow_mask)
-            for i in indices_to_combine:
-                cv2.drawContours(yellow_mask_combined, yellow_contours, i, 255, thickness=cv2.FILLED)
-
-            for i in indices_to_combine:
-                cnt = yellow_contours[i]
+            for cnt in yellow_contours:
                 approx = cv2.approxPolyDP(cnt, 0.03 * cv2.arcLength(cnt, True), True)
                 if len(approx) == 6:
                     rect = cv2.minAreaRect(cnt)
@@ -70,10 +44,12 @@ class ShapeRecognition:
                     cv2.drawContours(green_roi, [box], 0, (0, 255, 0), 2)
                     M = cv2.moments(cnt)
                     if M['m00'] != 0:
-                        cx = int(M['m10'] / M['m00'])
-                        cy = int(M['m01'] / M['m00'])
-                        cv2.putText(frame, 'ARROW', (x + cx, y + cy), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
-                        self.arrows.append((cx, cy, "ARROW"))
+                        cx = int(M['m10']/M['m00'])
+                        cy = int(M['m01']/M['m00'])
+                        if cy > max_flag_y:
+                            max_flag_y = cy  # 가장 높은 FLAG의 y 좌표 업데이트
+                            flag_detected = True
+                            self.arrows.append((cx, cy, "ARROW"))
                 else:
                     rect = cv2.minAreaRect(cnt)
                     box = cv2.boxPoints(rect)
@@ -81,14 +57,16 @@ class ShapeRecognition:
                     cv2.drawContours(green_roi, [box], 0, (0, 255, 0), 2)
                     M = cv2.moments(cnt)
                     if M['m00'] != 0:
-                        cx = int(M['m10'] / M['m00'])
-                        cy = int(M['m01'] / M['m00'])
-                        cv2.putText(frame, 'FLAG', (x + cx, y + cy), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
-                        self.flags.append((cx, cy, "FLAG"))
-                        flag_detected = True
+                        cx = int(M['m10']/M['m00'])
+                        cy = int(M['m01']/M['m00'])
+                        if cy > max_flag_y:
+                            max_flag_y = cy  # 가장 높은 FLAG의 y 좌표 업데이트
+                            flag_detected = True
+                            self.flags.append((cx, cy, "FLAG"))
 
             if flag_detected:
                 cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 0, 255), 2)
+                cv2.putText(frame, 'FLAG', (x + cx, y + max_flag_y), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
 
         return frame
 
@@ -101,6 +79,7 @@ class ShapeRecognition:
 
             frame = self.process_frame(frame)
 
+            # Display the original frame
             cv2.imshow('Frame', frame)
 
             key = cv2.waitKey(1) & 0xFF
