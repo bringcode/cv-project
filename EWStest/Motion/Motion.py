d446ce053f908a08e389ae97a462ae2fd57a4c23
HyunWoo Cho <hwooo2255@gmail.com>
2023-11-07 20:10:21 +0900

    Shot Debug

--- EWStest/Motion/Motion.py
+++ EWStest/Motion/Motion.py
@@ -223,11 +223,11 @@ class Motion:
         if dir == "LEFT":
             print("왼쪽에서 치겠습니다.")
             self.TX_data_py3(170)
-            Motion.TX_data_py3(170)
+            self.TX_data_py3(170)
         elif dir == "RIGHT":
             print("오른쪽에서 치겠습니다.")
             self.TX_data_py3(171)
-            Motion.TX_data_py3(171)
+            self.TX_data_py3(171)
 
     # 1도씩 set_head하기 (174~191)
     def set_head_small(self, dir, angle=0):
