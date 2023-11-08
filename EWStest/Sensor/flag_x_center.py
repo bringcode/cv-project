6ba34b177493a0a4fd7d459b962060fd53806f99
bringcode <79398104+bringcode@users.noreply.github.com>
2023-11-08 22:11:04 +0900

    Update flag_x_center.py

--- EWStest/Sensor/flag_x_center.py
+++ EWStest/Sensor/flag_x_center.py
@@ -95,5 +95,5 @@ class FlagxCenterMeasurer:
 
 if __name__ == "__main__":
     video_path = 0  # 웹캠을 사용하려면 0을 사용
-    shape_recognition = FlagxCenterMeasurer(video_path)
+    shape_recognition = FlagxCenterMeasurer(video_path, img_width=640, img_height=480)
     shape_recognition.run()
