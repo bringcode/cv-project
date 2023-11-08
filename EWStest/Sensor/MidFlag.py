b57d224ba1ceb99e884975e9a542a1114864a0a8
daeheejang <84303422+daeheejang@users.noreply.github.com>
2023-11-08 19:36:22 +0900

    Update MidFlag.py

--- EWStest/Sensor/MidFlag.py
+++ EWStest/Sensor/MidFlag.py
@@ -9,62 +9,6 @@ class ShapeRecognition:
         self.green_boxes = []
         self.farthest_flag_boxes = []  # 모든 flag의 중점값을 저장하는 리스트
 
-    def process_frame(self, frame):
-        hsv_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-
-        # 녹색 범위 정의
-        low_green = np.array([57, 78, 61])
-        high_green = np.array([89, 255, 255])
-        green_mask = cv2.inRange(hsv_frame, low_green, high_green)
-        result_frame = cv2.bitwise_and(frame, frame, mask=green_mask)
-        contours, _ = cv2.findContours(green_mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-        self.green_boxes = [cv2.boundingRect(contour) for contour in contours]
-
-        # 노랑색 범위 정의
-        low_yellow = np.array([0, 16, 144])
-        high_yellow = np.array([43, 184, 255])
-        yellow_mask = cv2.inRange(hsv_frame, low_yellow, high_yellow)
-
-        for green_box in self.green_boxes:
-            x, y, w, h = green_box
-            green_roi = frame[y:y+h, x:x+w]
-            yellow_roi_mask = yellow_mask[y:y+h, x:x+w]
-            yellow_contours, _ = cv2.findContours(yellow_roi_mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-            
-            # flag의 중점값을 저장하는 리스트
-            flag_centers = []
-
-            for cnt in yellow_contours:
-                # 영역의 면적 계산
-                area = cv2.contourArea(cnt)
-                if area > 10:  # 일정 면적 이상의 영역만 처리
-                    rect = cv2.minAreaRect(cnt)
-                    box = cv2.boxPoints(rect)
-                    box = np.int0(box)
-                    cv2.drawContours(green_roi, [box], 0, (0, 255, 0), 2)
-                    M = cv2.moments(cnt)
-                    if M['m00'] != 0:
-                        cx = int(M['m10'] / M['m00'])
-                        cy = int(M['m01'] / M['m00'])
-                        cv2.putText(frame, 'Flag', (x+cx, y+cy), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
-                    
-                        # flag_centers 리스트에 중점값 추가
-                        flag_centers.append((cx, cy))
-
-            # flag_centers가 비어있지 않을 때만 실행
-            if flag_centers:
-                # flag_centers 리스트에서 중점값이 가장 높은 flag 선택
-                farthest_flag_center = min(flag_centers, key=lambda center: center[1])
-                # 해당 flag의 박스 그리기
-                cv2.rectangle(green_roi, (farthest_flag_center[0] - 10, farthest_flag_center[1] - 10),
-                              (farthest_flag_center[0] + 10, farthest_flag_center[1] + 10), (0, 0, 255), 2)
-                cv2.putText(frame, 'Farthest Flag', (x + farthest_flag_center[0], y + farthest_flag_center[1]),
-                            cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
-                # farthest_flag_boxes 리스트에 중점값과 "FLAG" 추가
-                self.farthest_flag_boxes.append((x + farthest_flag_center[0], y + farthest_flag_center[1], "FLAG"))
-
-        return frame
-
     def run(self):
         while True:
             ret, frame = self.cap.read()
@@ -72,7 +16,58 @@ class ShapeRecognition:
                 print("Failed to grab a frame")
                 break
 
-            frame = self.process_frame(frame)
+            hsv_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+
+            # 녹색 범위 정의
+            low_green = np.array([57, 78, 61])
+            high_green = np.array([89, 255, 255])
+            green_mask = cv2.inRange(hsv_frame, low_green, high_green)
+            result_frame = cv2.bitwise_and(frame, frame, mask=green_mask)
+            contours, _ = cv2.findContours(green_mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+            self.green_boxes = [cv2.boundingRect(contour) for contour in contours]
+
+            # 노랑색 범위 정의
+            low_yellow = np.array([0, 16, 144])
+            high_yellow = np.array([43, 184, 255])
+            yellow_mask = cv2.inRange(hsv_frame, low_yellow, high_yellow)
+
+            for green_box in self.green_boxes:
+                x, y, w, h = green_box
+                green_roi = frame[y:y+h, x:x+w]
+                yellow_roi_mask = yellow_mask[y:y+h, x:x+w]
+                yellow_contours, _ = cv2.findContours(yellow_roi_mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+                # flag의 중점값을 저장하는 리스트
+                flag_centers = []
+
+                for cnt in yellow_contours:
+                    # 영역의 면적 계산
+                    area = cv2.contourArea(cnt)
+                    if area > 10:  # 일정 면적 이상의 영역만 처리
+                        rect = cv2.minAreaRect(cnt)
+                        box = cv2.boxPoints(rect)
+                        box = np.int0(box)
+                        cv2.drawContours(green_roi, [box], 0, (0, 255, 0), 2)
+                        M = cv2.moments(cnt)
+                        if M['m00'] != 0:
+                            cx = int(M['m10'] / M['m00'])
+                            cy = int(M['m01'] / M['m00'])
+                            cv2.putText(frame, 'Flag', (x+cx, y+cy), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
+
+                            # flag_centers 리스트에 중점값 추가
+                            flag_centers.append((cx, cy))
+
+                # flag_centers가 비어있지 않을 때만 실행
+                if flag_centers:
+                    # flag_centers 리스트에서 중점값이 가장 높은 flag 선택
+                    farthest_flag_center = min(flag_centers, key=lambda center: center[1])
+                    # 해당 flag의 박스 그리기
+                    cv2.rectangle(green_roi, (farthest_flag_center[0] - 10, farthest_flag_center[1] - 10),
+                                  (farthest_flag_center[0] + 10, farthest_flag_center[1] + 10), (0, 0, 255), 2)
+                    cv2.putText(frame, 'Farthest Flag', (x + farthest_flag_center[0], y + farthest_flag_center[1]),
+                                cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
+                    # farthest_flag_boxes 리스트에 중점값과 "FLAG" 추가
+                    self.farthest_flag_boxes.append((x + farthest_flag_center[0], y + farthest_flag_center[1], "FLAG"))
 
             # Display the original frame
             cv2.imshow('Frame', frame)
