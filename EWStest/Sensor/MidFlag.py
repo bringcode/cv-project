f7e2d910f77640bf75c746911527c8b29bbe8e79
daeheejang <84303422+daeheejang@users.noreply.github.com>
2023-11-08 19:09:28 +0900

    Update MidFlag.py

--- EWStest/Sensor/MidFlag.py
+++ EWStest/Sensor/MidFlag.py
@@ -6,15 +6,6 @@ class ShapeRecognition:
         self.cap = cv2.VideoCapture(video_path, cv2.CAP_V4L)
         if not self.cap.isOpened():
             raise ValueError(f"Video at {video_path} cannot be opened")
-        self.green_boxes = []
-
-    def find_highest_box(self, boxes):
-        # 중점 y 좌표를 기준으로 가장 높은 위치의 박스를 찾습니다.
-        if not boxes:
-            return None
-
-        highest_box = min(boxes, key=lambda box: box[1])
-        return highest_box
 
     def process_frame(self, frame):
         hsv_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
@@ -25,19 +16,18 @@ class ShapeRecognition:
         green_mask = cv2.inRange(hsv_frame, low_green, high_green)
         result_frame = cv2.bitwise_and(frame, frame, mask=green_mask)
         contours, _ = cv2.findContours(green_mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-        self.green_boxes = [cv2.boundingRect(contour) for contour in contours]
 
-        # 노랑색 범위 정의
-        low_yellow = np.array([0, 16, 144])
-        high_yellow = np.array([43, 184, 255])
-        yellow_mask = cv2.inRange(hsv_frame, low_yellow, high_yellow)
-
-        for green_box in self.green_boxes:
-            x, y, w, h = green_box
+        for contour in contours:
+            x, y, w, h = cv2.boundingRect(contour)
             green_roi = frame[y:y+h, x:x+w]
-            yellow_roi_mask = yellow_mask[y:y+h, x:x+w]
-            yellow_contours, _ = cv2.findContours(yellow_roi_mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-            
+
+            # 초록 박스 내부에 해당하는 노랑색 영역만 처리
+            yellow_mask = np.zeros_like(green_roi)
+            yellow_contours = []
+            for cnt in contours:
+                if x < cnt[0][0][0] < x + w and y < cnt[0][0][1] < y + h:
+                    yellow_contours.append(cnt)
+
             for cnt in yellow_contours:
                 # 영역의 면적 계산
                 area = cv2.contourArea(cnt)
@@ -51,15 +41,6 @@ class ShapeRecognition:
                         cx = int(M['m10'] / M['m00'])
                         cy = int(M['m01'] / M['m00'])
                         cv2.putText(frame, 'Flag', (x+cx, y+cy), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
-
-            # 가장 높은 위치의 박스를 찾습니다.
-            target_box = self.find_highest_box(self.green_boxes)
-
-            if target_box is not None:
-                # 타겟 박스를 빨강색으로 표시
-                x, y, w, h = target_box
-                cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 0, 255), 2)
-                cv2.putText(frame, 'Target', (x + w // 2, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
 
         return frame
 
