c4a0bc2b7ec09d2ce56afe4edda7e8b76ccc4744
daeheejang <84303422+daeheejang@users.noreply.github.com>
2023-11-08 19:33:55 +0900

    Update MidFlag.py

--- EWStest/Sensor/MidFlag.py
+++ EWStest/Sensor/MidFlag.py
@@ -7,6 +7,7 @@ class ShapeRecognition:
         if not self.cap.isOpened():
             raise ValueError(f"Video at {video_path} cannot be opened")
         self.green_boxes = []
+        self.farthest_flag_boxes = []  # 모든 flag의 중점값을 저장하는 리스트
 
     def process_frame(self, frame):
         hsv_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
@@ -24,14 +25,12 @@ class ShapeRecognition:
         high_yellow = np.array([43, 184, 255])
         yellow_mask = cv2.inRange(hsv_frame, low_yellow, high_yellow)
 
-        farthest_flag_center = None  # 가장 먼 flag의 중점값을 저장하는 변수
-
         for green_box in self.green_boxes:
             x, y, w, h = green_box
             green_roi = frame[y:y+h, x:x+w]
             yellow_roi_mask = yellow_mask[y:y+h, x:x+w]
             yellow_contours, _ = cv2.findContours(yellow_roi_mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-
+            
             # flag의 중점값을 저장하는 리스트
             flag_centers = []
 
@@ -48,36 +47,32 @@ class ShapeRecognition:
                         cx = int(M['m10'] / M['m00'])
                         cy = int(M['m01'] / M['m00'])
                         cv2.putText(frame, 'Flag', (x+cx, y+cy), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
-
+                    
                         # flag_centers 리스트에 중점값 추가
                         flag_centers.append((cx, cy))
 
             # flag_centers가 비어있지 않을 때만 실행
             if flag_centers:
                 # flag_centers 리스트에서 중점값이 가장 높은 flag 선택
-                farthest_flag_center_candidate = max(flag_centers, key=lambda center: center[1])
+                farthest_flag_center = max(flag_centers, key=lambda center: center[1])
+                # 해당 flag의 박스 그리기
+                cv2.rectangle(green_roi, (farthest_flag_center[0] - 10, farthest_flag_center[1] - 10),
+                              (farthest_flag_center[0] + 10, farthest_flag_center[1] + 10), (0, 0, 255), 2)
+                cv2.putText(frame, 'Farthest Flag', (x + farthest_flag_center[0], y + farthest_flag_center[1]),
+                            cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
+                # farthest_flag_boxes 리스트에 중점값과 "FLAG" 추가
+                self.farthest_flag_boxes.append((x + farthest_flag_center[0], y + farthest_flag_center[1], "FLAG"))
 
-                # 최초로 찾은 flag 또는 더 멀리에 있는 flag 선택
-                if farthest_flag_center is None or farthest_flag_center_candidate[1] > farthest_flag_center[1]:
-                    farthest_flag_center = farthest_flag_center_candidate
-
-        if farthest_flag_center is not None:
-            x, y = farthest_flag_center
-            # 빨간 박스 그리기
-            cv2.rectangle(frame, (x - 10, y - 10), (x + 10, y + 10), (0, 0, 255), 2)
-
-        return frame, farthest_flag_center
+        return frame
 
     def run(self):
-        farthest_flag_center = None
-
         while True:
             ret, frame = self.cap.read()
             if not ret:
                 print("Failed to grab a frame")
                 break
 
-            frame, farthest_flag_center = self.process_frame(frame)
+            frame = self.process_frame(frame)
 
             # Display the original frame
             cv2.imshow('Frame', frame)
@@ -86,14 +81,14 @@ class ShapeRecognition:
             if key == ord('q'):
                 break
 
+        if self.farthest_flag_boxes:
+            for box in self.farthest_flag_boxes:
+                print(f"Farthest Flag Center: {box[0]}, {box[1]}")
+
         self.cap.release()
         cv2.destroyAllWindows()
-
-        return farthest_flag_center
 
 if __name__ == "__main__":
     video_path = 0  # Use 0 for webcam
     shape_recognition = ShapeRecognition(video_path)
-    farthest_flag_center = shape_recognition.run()
-    if farthest_flag_center:
-        print(f"Farthest Flag Center: {farthest_flag_center}")
+    shape_recognition.run()
