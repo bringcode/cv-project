742e4964366777936d739937e385c0412ae5a6af
HyunWoo Cho <hwooo2255@gmail.com>
2023-11-10 19:13:27 +0900

    test

--- EWStest/Core/Controller.py
+++ EWStest/Core/Controller.py
@@ -27,7 +27,7 @@ class Act(Enum):
 # 상황 판단 하는 파트
 class Controller:
     robo: Robo = Robo()
-    act: Act = Act.SEARCH_PUTTING_LOCATION  # 순서도 시작
+    act: Act = Act.START # 순서도 시작
     # test START로 바꿔야함.
 
     count_putting: int = 0  # 퍼팅 횟수
