3d1ad2bf0b4a0f3aba21803656bafd3cc820487f
daeheejang <84303422+daeheejang@users.noreply.github.com>
2023-11-10 19:32:46 +0900

    Update ball_x_center.py

--- EWStest/Sensor/ball_x_center.py
+++ EWStest/Sensor/ball_x_center.py
@@ -138,7 +138,7 @@ class BallxCenterMeasurer:
                     ball_y = round((max_y + min_y / 2), 2)
 
 
-            # cv2.imshow('Object Dist Measure ', img)
+            cv2.imshow('Object Dist Measure ', img)
             # print(ball_x_isMiddle)
             # if cv2.waitKey(1) & 0xFF == ord('q'):
             #     cv2.destroyAllWindows()
