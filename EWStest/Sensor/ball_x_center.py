adf4b3f4ceb3d35a64ad6887e3e4415fb982ed3f
bringcode <79398104+bringcode@users.noreply.github.com>
2023-11-10 12:24:04 +0900

    Update ball_x_center.py

--- EWStest/Sensor/ball_x_center.py
+++ EWStest/Sensor/ball_x_center.py
@@ -135,6 +135,7 @@ class BallxCenterMeasurer:
                     ball_y = round((max_y + min_y / 2), 2)
 
             cv2.imshow('Object Dist Measure ', img)
+            print(ball_x_isMiddle)
             if cv2.waitKey(1) & 0xFF == ord('q'):
                 cv2.destroyAllWindows()
                 break
