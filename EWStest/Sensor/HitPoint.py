4419d924f4f9943c984d2b39cc9d8ee2f0c5dd86
daeheejang <84303422+daeheejang@users.noreply.github.com>
2023-11-11 14:57:42 +0900

    Update HitPoint.py

--- EWStest/Sensor/HitPoint.py
+++ EWStest/Sensor/HitPoint.py
@@ -84,17 +84,28 @@ class HitPointer:
 
         
         if np.degrees(m) <= 90: # 타격지점이 삼각형 밖에 위치
-            
-            x = self.calculate_out_x(m)
-            z = self.calculate_out_z(x)
-            angle_triangle = int(np.degrees(self.calculate_out_angle(x,m)))
-            judge_triangle = False
+            if(c!=0):
+                x = self.calculate_out_x(m)
+                z = self.calculate_out_z(x)
+                angle_triangle = int(np.degrees(self.calculate_out_angle(x,m)))
+                judge_triangle = False
+            else
+                x=self.calculate_zero_x()
+                y=self.calculate_zero_Z()
+                angle_triangle = int(np.degrees(self.calculate_zero_angle(x,m)))
+                judge_triangle = False
         
         else:                   # 타격지점이 삼각형 안에 위치
-            x = self.calculate_in_x(m)
-            z = self.calculate_in_z(x)
-            angle_triangle = int(np.degrees(self.calculate_in_angle(x,m)))
-            judge_triangle = True                      
+            if(c!=0):
+                x = self.calculate_in_x(m)
+                z = self.calculate_in_z(x)
+                angle_triangle = int(np.degrees(self.calculate_in_angle(x,m)))
+                judge_triangle = True
+            else
+                x=self.calculate_zero_x()
+                y=self.calculate_zero_Z()
+                angle_triangle = int(np.degrees(self.calculate_zero_angle(x,m)))
+                judge_triangle = True                  
           
         z_deg = np.degrees(z)  # z를 도(degree) 단위로 변환
         
