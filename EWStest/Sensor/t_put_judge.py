bc5dfddf23e1ee38e6f05ba44e4b64f3d00fc312
chloebh9 <chloebh9@naver.com>
2023-11-11 21:39:42 +0900

    Update

--- EWStest/Sensor/t_put_judge.py
+++ EWStest/Sensor/t_put_judge.py
@@ -83,13 +83,12 @@ class BallCenterMeasurer:
             upper = np.array([255, 255, 255])
             lower1 = np.array([0, 66, 87])
             upper1 = np.array([14, 255, 255])
-            lower2 = np.array([168, 0, 0])
-            upper2 = np.array([255, 255, 255])
+            # lower2 = np.array([168, 0, 0])
+            # upper2 = np.array([255, 255, 255])
             
             mask1 = cv2.inRange(hsv_img, lower, upper)
             mask2 = cv2.inRange(hsv_img, lower1, upper1)
-            mask3 = cv2.inRange(hsv_img, lower2, upper2)
-            
+            # mask3 = cv2.inRange(hsv_img, lower2, upper2)
 
             mask = mask1+mask2
 
