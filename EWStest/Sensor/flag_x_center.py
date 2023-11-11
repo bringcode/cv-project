e0c684b398ecb3b21f4ede7d2a5d74af52fe4130
bringcode <79398104+bringcode@users.noreply.github.com>
2023-11-11 17:40:21 +0900

    Update flag_x_center.py

--- EWStest/Sensor/flag_x_center.py
+++ EWStest/Sensor/flag_x_center.py
@@ -65,6 +65,7 @@ class FlagxCenterMeasurer:
             low_yellow = np.array([0, 16, 144])
             high_yellow = np.array([43, 184, 255])
             yellow_mask = cv2.inRange(hsv_frame, low_yellow, high_yellow)
+            max_x, min_x = 0,0 # 깃발을 못 찾았을 때 오류나는 것을 방지하기 위해 바운딩 박스의 좌표를 0으로 초기화
 
             for green_box in self.green_boxes:
                 x, y, w, h = green_box
@@ -108,6 +109,9 @@ class FlagxCenterMeasurer:
         #     max_x, min_x, max_y, min_y = self.max_x, self.min_x, self.max_y, self.min_y
 
         flag_x_isMiddle = self.judgeMiddle(max_x, min_x)
+        if max_x == 0 and min_x == 0:
+            print('홀컵이 잡히지 않음')
+            return ['N', 'N', 'N']
         return [flag_x_isMiddle, farthest_flag_center[0], farthest_flag_center[1]]
 
 
