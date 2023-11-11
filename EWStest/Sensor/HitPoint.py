834a998e60b367b9db78cd8290c556573ac061dd
daeheejang <84303422+daeheejang@users.noreply.github.com>
2023-11-11 15:11:54 +0900

    Update HitPoint.py

--- EWStest/Sensor/HitPoint.py
+++ EWStest/Sensor/HitPoint.py
@@ -90,7 +90,7 @@ class HitPointer:
                 z = self.calculate_out_z(x)
                 angle_triangle = int(np.degrees(self.calculate_out_angle(x,m)))
                 judge_triangle = False
-            else
+            else:
                 c = self.a  - self.b
                 x=self.calculate_zero_x()
                 y=self.calculate_zero_Z()
