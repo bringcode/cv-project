43fc0f4559eacacce0a52cc4f11bf7517d1a5cf8
daeheejang <84303422+daeheejang@users.noreply.github.com>
2023-11-11 14:07:59 +0900

    Update ball_y_center.py

--- EWStest/Sensor/ball_y_center.py
+++ EWStest/Sensor/ball_y_center.py
@@ -59,7 +59,7 @@ class BallyCenterMeasurer:
             self.img_height - max_y
         )  # r_dist: 공을 표시한 박스 가장 오른쪽으로부터 영상 가장 오른쪽 끝까지의 거리
 
-        error_range = 20  # 오차 허용 범위
+        error_range = 30  # 오차 허용 범위
 
         # 박스가 영상의 왼쪽 오른쪽 끝 부분과 떨어진 거리가 오차 허용 범위(error_range) 이내일 때, True를 is_Middle에 저장
         is_Middle = abs(up_dist - down_dist) < error_range
