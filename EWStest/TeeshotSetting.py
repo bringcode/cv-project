710b5a05cdbafcac097f7a744563a0d2059a5678
bringcode <79398104+bringcode@users.noreply.github.com>
2023-11-15 21:47:11 +0900

    TeeshotSetting.py: 3도씩 내리게 수정

--- EWStest/TeeshotSetting.py
+++ EWStest/TeeshotSetting.py
@@ -147,8 +147,8 @@ class BallCenterMeasurer:
             cv2.imshow("Object Dist Measure ", img)
 
             if cv2.waitKey(1) & 0xFF == ord("n"):
-                self.robo._motion.set_head_small("DOWN", 1)
-                angle -= 1
+                self.robo._motion.set_head_small("DOWN", 3)
+                angle -= 3
                 continue
 
             if cv2.waitKey(1) & 0xFF == ord("q"):
